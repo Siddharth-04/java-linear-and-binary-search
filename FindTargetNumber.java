8a1f5363c6b7c4ba814fbee8c5c62e792bf46593
Siddharth <siddharth1360.be21@chitkara.edu.in>
2025-03-25 13:25:35 +0530

    [Siddharth] ADD: Find target element in an array

---
+++ FindTargetNumber.java
@@ -0,0 +1,25 @@
+public class FindTargetNumber {
+    public static int findTargetElement(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while(left <= right){
+            int mid = left  + (right-left)/2;
+            if(arr[mid] == target){
+                return mid;
+            }
+            else if(arr[mid] > target){
+                right = mid - 1;
+            }
+            else{
+                left = mid + 1;
+            }
+        }
+
+        return -1;
+    }
+    public static void main(String[] args) {
+        int [] arr = {1,2,3,4,5,7,8,9};
+        System.out.println(findTargetElement(arr,4));
+    }
+}
